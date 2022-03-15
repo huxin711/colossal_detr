4511f61cd366d008b7b8d3f706606768eb633d95
huxin711 <dr.huxin711@foxmail.com>
2022-03-15 21:45:09 +0800

    Params File

--- util/detr_1d.py
+++ util/detr_1d.py
@@ -1,4 +1,4 @@
-BATCH_SIZE = 256
+BATCH_SIZE = 128
 LEARNING_RATE = 2e-3
 WEIGHT_DECAY = 3e-2
 
@@ -16,7 +16,7 @@ parallel = dict(
 
 seed = 77
 
-LOG_PATH = f"./detr_{TENSOR_PARALLEL_MODE}_ai2d_tp{TENSOR_PARALLEL_SIZE}_bs{BATCH_SIZE}_lr{LEARNING_RATE}/"
+LOG_PATH = f"./detr_{TENSOR_PARALLEL_MODE}_ai2d_{TENSOR_PARALLEL_SIZE}_bs{BATCH_SIZE}_lr{LEARNING_RATE}/"
 
 
 find_unused_parameters = True
